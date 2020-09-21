b357411a2a64df00056e91f00ec9f63b3aabf193
Dhinendra Sean Rajapakse <Lightning117>
2020-09-21 13:13:13 +0530

    Created prototype to use angular 7 and compass plugin.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CesiumNav';
+  viewer: any;
+  constructor(private el: ElementRef) {
+  }
+  ngOnInit(): void {
+    this.viewer = new Cesium.Viewer(this.el.nativeElement, {});
+    this.viewer.extend(Cesium.viewerCesiumNavigationMixin);
+  }
+
 }
